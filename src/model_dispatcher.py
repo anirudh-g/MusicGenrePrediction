ef76f4e670f001e2efcc7e12ce1cff45254f3897
Anirudh G <g.anirudh15@gmail.com>
2021-08-11 20:16:12 +0530

    penultimate_day_end

--- src/model_dispatcher.py
+++ src/model_dispatcher.py
@@ -1,12 +1,16 @@
 from sklearn import ensemble
-
+from sklearn import linear_model
+from xgboost import XGBClassifier
+from lightgbm import LGBMClassifier
 estimators = [
-        ('bagging', ensemble.RandomForestClassifier(random_state=42, class_weight='balanced', bootstrap=True)), 
-        ('boosting', ensemble.GradientBoostingClassifier(random_state=42))
+        ('rf', ensemble.RandomForestClassifier(random_state=42, class_weight='balanced', bootstrap=True)),
+        ('lgbm', LGBMClassifier(class_weight='balanced', random_state=42, n_jobs=-1)),
+       # ('et', ensemble.ExtraTreesClassifier(class_weight='balanced', bootstrap=True, random_state=42))
     ]
 
 stack_model = ensemble.StackingClassifier(
-                    estimators=estimators, 
-                    final_estimator=ensemble.ExtraTreesClassifier(
-                        random_state=42, class_weight='balanced', bootstrap=True), cv=5, verbose=2)
+                estimators=estimators, 
+                final_estimator=linear_model.LogisticRegression(
+                penalty='l1', class_weight='balanced', solver='liblinear',
+                 random_state=42), cv=5, verbose=2)
                         
--- src/test.py
+++ src/test.py
@@ -6,7 +6,6 @@ import joblib
 test_data = pd.read_csv(config.TEST_FINAL)
 sub_temp = pd.read_csv(config.submission)
 
-
 trained_model = joblib.load(config.TRAINED_MODEL)
 finalpreds = pd.DataFrame(trained_model.predict_proba(test_data), columns=list(sub_temp.columns))
 print('Finished running the model. Check data->submission.csv for the output')
