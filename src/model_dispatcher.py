a750236b15f2eeb6446875aba4edf0a6c6b44891
Anirudh G <g.anirudh15@gmail.com>
2021-08-13 10:08:48 +0530

    final_update

--- src/model_dispatcher.py
+++ src/model_dispatcher.py
@@ -4,8 +4,7 @@ from xgboost import XGBClassifier
 from lightgbm import LGBMClassifier
 estimators = [
         ('rf', ensemble.RandomForestClassifier(random_state=42, class_weight='balanced', bootstrap=True)),
-        ('lgbm', LGBMClassifier(class_weight='balanced', random_state=42, n_jobs=-1)),
-       # ('et', ensemble.ExtraTreesClassifier(class_weight='balanced', bootstrap=True, random_state=42))
+        ('xgb', XGBClassifier(random_state=42))
     ]
 
 stack_model = ensemble.StackingClassifier(
